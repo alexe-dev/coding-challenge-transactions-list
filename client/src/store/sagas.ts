73ae55ba16063a3e06b014097d7dfbd0aa1fafc7
Alex Alexeev <work2alex@gmail.com>
2023-10-14 16:36:36 +0200

    fix: use dev wallet in SEND_TRANSACTION saga

--- client/src/store/sagas.ts
+++ client/src/store/sagas.ts
@@ -1,5 +1,5 @@
 import { takeEvery } from 'redux-saga/effects';
-import { JsonRpcProvider, Transaction, TransactionResponse, TransactionReceipt, BrowserProvider, Signer } from 'ethers';
+import { JsonRpcProvider, Transaction, TransactionResponse, TransactionReceipt, Wallet, parseEther } from 'ethers';
 
 import apolloClient from '../apollo/client';
 import { Actions } from '../types';
@@ -11,9 +11,14 @@ function* sendTransaction() {
   // this could have been passed along in a more elegant fashion,
   // but for the purpouses of this scenario it's good enough
   // @ts-ignore
-  const walletProvider = new BrowserProvider(window.web3.currentProvider);
 
-  const signer: Signer = yield walletProvider.getSigner();
+  // copied dev wallet config from generateTranactions.js
+  // otherwise it uses real wallet (metamask)
+
+  const walletFromMnemonic = Wallet.fromPhrase(
+    'myth like bonus scare over problem client lizard pioneer submit female collect'
+  );
+  const signer = new Wallet(walletFromMnemonic.privateKey, provider);
 
   const accounts: Array<{ address: string }> = yield provider.listAccounts();
 
@@ -26,11 +31,12 @@ function* sendTransaction() {
 
   const transaction = {
     to: randomAddress(),
-    value: 1000000000000000000,
+    value: parseEther((Math.random() * (10 - 0.5) + 0.5).toString()),
   };
 
   try {
     const txResponse: TransactionResponse = yield signer.sendTransaction(transaction);
+
     const response: TransactionReceipt = yield txResponse.wait();
 
     const receipt: Transaction = yield response.getTransaction();
@@ -38,25 +44,24 @@ function* sendTransaction() {
     const variables = {
       transaction: {
         gasLimit: (receipt.gasLimit && receipt.gasLimit.toString()) || '0',
-        gasPrice: (receipt.gasPrice && receipt.gasPrice.toString())|| '0',
+        gasPrice: (receipt.gasPrice && receipt.gasPrice.toString()) || '0',
         to: receipt.to,
         from: receipt.from,
         value: (receipt.value && receipt.value.toString()) || '',
         data: receipt.data || null,
         chainId: (receipt.chainId && receipt.chainId.toString()) || '123456',
         hash: receipt.hash,
-      }
+      },
     };
 
     yield apolloClient.mutate({
       mutation: SaveTransaction,
       variables,
     });
-
   } catch (error) {
-    //
+    // TODO: send to sentry or similar
+    console.log(error);
   }
-
 }
 
 export function* rootSaga() {
